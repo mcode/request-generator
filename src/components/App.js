0479a77cb89992b42aa423637a41d62e3f86154b
Robert A Dingwell <bobd@mitre.org>
2023-11-01 12:32:48 -0400

    Adding rewire and build.js to create unminified production builds as the minified version does not work.  Also switched app to use HashRouter to allow the app to run on github pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import React, {Component} from 'react';
 import RequestBuilder from '../containers/RequestBuilder';
 import PatientPortal from '../containers/PatientPortal';
 import theme from '../containers/styles/theme';
