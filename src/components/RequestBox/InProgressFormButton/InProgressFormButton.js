4dd6fb568f963651b68bb3d282c7a32a10f42a26
Zach Robin <zrobin144@gmail.com>
2023-12-08 07:29:47 -0800

    addressed comments, bug fixes

--- src/components/RequestBox/InProgressFormButton/InProgressFormButton.js
+++ src/components/RequestBox/InProgressFormButton/InProgressFormButton.js
@@ -4,29 +4,26 @@ import './InProgressFormButtonStyle.css';
 
 export default function InProgressFormButton(props) {
     return (
-        <Paper variant='elevation' className='inprogress-container' sx={{ bgcolor: '#F3F6F9' }}>
-            {props.qrResponse.questionnaire ? (
-                <Box>
-                    <Typography variant="h6" color="text.secondary" gutterBottom>
+        props.qrResponse.questionnaire ? (
+                <Box className={'inprogress-container'}>
+                    <Typography variant='h6' color='text.primary' gutterBottom>
                         In Progress Form
                     </Typography>
-                    <Typography variant="subtitle1">
-                        <Typography color="text.secondary">  Author: </Typography>{props.qrResponse.author ? props.qrResponse.author.reference : 'empty'}
+                    <Typography variant='subtitle1'>
+                        <Typography color='text.secondary'>  Practitioner: </Typography>{props.qrResponse.author ? props.qrResponse.author.reference : 'empty'}
                     </Typography>
-                    <Typography variant="subtitle1" >
-                        <Typography color="text.secondary">  Last Edited: </Typography> {props.qrResponse.authored ? props.qrResponse.authored : 'empty'}
+                    <Typography variant='subtitle1' >
+                        <Typography color='text.secondary'>  Last Edited: </Typography> {props.qrResponse.authored ? props.qrResponse.authored : 'empty'}
                     </Typography>
-                    <Typography sx={{ mb: 4 }} variant="subtitle1">
-                        <Typography color="text.secondary">  Form Link: </Typography>{props.qrResponse.questionnaire ? props.qrResponse.questionnaire : 'empty'}
+                    <Typography sx={{ mb: 4 }} variant='subtitle1'>
+                        <Typography color='text.secondary'>  Form Link: </Typography>{props.qrResponse.questionnaire ? props.qrResponse.questionnaire : 'empty'}
                     </Typography>
-                    <ButtonGroup variant="contained" aria-label="button group">
-                        <Button onClick={props.relaunch} color='secondary' varient='contained'>
+                    <ButtonGroup variant='outlined' aria-label='button group'>
+                        <Button onClick={props.relaunch} color='primary' varient='outlined' style={{ width: '300px' }}>
                             Open In-Progress Form
                         </Button>
                     </ButtonGroup>
                 </Box>
-            ) : <h4>Error: No in progress forn found</h4>
-            }
-        </Paper>
+        ) : ''
     );
 }
