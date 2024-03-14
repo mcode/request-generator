e39d1b9c24c10cf85fedd76ad737252decda3771
KeeyanGhoreshi <keeyan.ghoreshi@uconn.edu>
2024-03-13 20:57:28 -0400

    initial changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import dotenv from 'dotenv'
 
-dotenv.config() // load env vars from .env
 export default defineConfig({
     // depending on your application, base can also be "/"
     base: '',
-    plugins: [react(), viteTsconfigPaths(), nodePolyfills()],
-    define: {
-        'process.env': process.env
-    },
+    plugins: [react(), viteTsconfigPaths()],
     server: {    
+        // this ensures that the browser opens upon server start
+        open: true,
         // this sets a default port to 3000  
         port: 3000, 
-        open: false,
-        host: true
     },
 })
